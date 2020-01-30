d5cc3f77781762f65e04b7b95da1ac1760cc0228
thaisemanuele <thatitis@gmail.com>
2020-01-30 08:29:02 +0200

    basic signin form

---
+++ src/pages/signin-signup/signin-signup.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import SignIn from '../../components/signin/signin.component'; 
+import './signin-signup.styles.scss';
+
+const SignInAndSignUpPage = () => (
+    <div className="signin-signup">
+        <SignIn />
+    </div>
+);
+
+export default SignInAndSignUpPage;

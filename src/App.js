ead6245080b13fbf287492985fa73b3a58bc4298
thaisemanuele <thatitis@gmail.com>
2020-02-02 18:21:20 +0200

    Google sign in and sign out

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
@@ -6,19 +6,43 @@ import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/signin-signup/signin-signup.component';
 
+import { auth } from './firebase/firebase.utils';
+
 import './App.css';
 
-function App() {
-  return (
-    <div >
-      <Header />
-      <Switch>
-        <Route exact path="/" component={HomePage}/>
-        <Route path="/shop" component={ShopPage}/>
-        <Route path="/signin" component={SignInAndSignUpPage}/>
-      </Switch>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged( user => {
+      this.setState({currentUser: user});
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+  render() {
+    return (
+      <div >
+        <Header currentUser={this.state.currentUser}/>
+        <Switch>
+          <Route exact path="/" component={HomePage}/>
+          <Route path="/shop" component={ShopPage}/>
+          <Route path="/signin" component={SignInAndSignUpPage}/>
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;

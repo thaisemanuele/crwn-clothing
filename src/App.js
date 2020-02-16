fd63a206084db624e10b91643dfce21c5641f723
thaisemanuele <thatitis@gmail.com>
2020-02-16 11:54:19 +0200

    finishing sign in with email and password

--- src/App.js
+++ src/App.js
@@ -32,7 +32,6 @@ class App extends Component {
               ...snapshot.data()
             }
           });
-          console.log(this.state);
           
         });
       }
--- src/components/signin/signin.component.jsx
+++ src/components/signin/signin.component.jsx
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
-import { signInWithGoogle } from '../../firebase/firebase.utils';
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 import './signin.styles.scss';
 
 class SignIn extends Component {
@@ -15,9 +15,17 @@ class SignIn extends Component {
         }
     }
 
-    handleSubmit = event => {
+    handleSubmit = async event => {
         event.preventDefault();
-        this.setState({ email: '', password: ''});
+        const { email, password } = this.state;
+
+        try {
+            await auth.signInWithEmailAndPassword(email, password);
+            this.setState({ email: '', password: ''});
+        } catch (error) {
+            console.error(error);
+        }
+
     };
 
     handleChange = event => {
